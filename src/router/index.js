609a3e57061f32241be35d037fe22652fc003a73
MarkPoloChina <1228479724@qq.com>
2022-07-03 17:18:37 +0800

    [update] add ElementPlus & Electron.

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,16 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
 
 const routes = [
+  // {
+  //   path: '/',
+  //   name: 'home',
+  //   component: HomeView
+  // },
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'index',
+    component: () => import('../views/IcxorIndex.vue')
   },
   {
     path: '/about',
