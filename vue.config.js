1239c3c4bfa2294ccbc3c71eb040c99b3f181e59
MarkPoloChina <1228479724@qq.com>
2022-07-10 15:45:50 +0800

    [update] add sass-resources-loader.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'ImageComplexor'
+      return args
+    })
+    config.module.rule('scss').oneOfs.store.forEach(item => {
+      item
+        .use('sass-resources-loader')
+        .loader('sass-resources-loader')
+        .options({
+          resources: './src/style/global.scss'
+        })
+        .end()
+    });
+  }
 })
