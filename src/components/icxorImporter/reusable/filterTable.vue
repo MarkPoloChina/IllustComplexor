5a8b20448a296df87d6c537064aefa89877dc484
MarkPoloChina <1228479724@qq.com>
2023-08-16 01:32:32 +0800

    [refactor] use normal table for filterTable.

--- src/components/icxorImporter/reusable/filterTable.vue
+++ src/components/icxorImporter/reusable/filterTable.vue
@@ -1,53 +1,138 @@
 <template>
-  <el-auto-resizer>
-    <template #default="{ height, width }">
-      <el-table-v2
-        :columns="columns"
-        :data="writableList"
-        :height="height"
-        :width="width"
-        :row-class="
-          ({ rowIndex }) => {
-            switch (writableList[rowIndex].status) {
-              case 'ready':
-                return 'success-row';
-              case 'success':
-                return 'success-row';
-              case 'fault':
-                return 'danger-row';
-              case 'ignore':
-                return 'danger-row';
-              case 'conflict':
-                return 'warning-row';
-              default:
-                return '';
-            }
+  <div>
+    <el-table
+      :data="showList"
+      v-loading="loading"
+      style="width: 100%; height: calc(100% - 38px)"
+      :row-class-name="
+        ({ rowIndex }) => {
+          switch (showList[rowIndex].status) {
+            case 'ready':
+              return 'success-row';
+            case 'success':
+              return 'success-row';
+            case 'fault':
+              return 'danger-row';
+            case 'ignore':
+              return 'danger-row';
+            case 'conflict':
+              return 'warning-row';
+            default:
+              return '';
           }
-        "
-      >
-        <template #overlay v-if="loading">
+        }
+      "
+    >
+      <template #empty>
+        <el-empty :image-size="120" />
+      </template>
+      <el-table-column :width="50">
+        <template #default="{ row }">
+          <SelectionCell
+            :value="row.checked"
+            @change="
+              (value) => {
+                if (row.status == 'ready' || rowData.status == 'conflict') {
+                  row.checked = value;
+                }
+              }
+            "
+            :disabled="row.status != 'ready' && row.status != 'conflict'"
+          />
+        </template>
+        <template #header>
+          <SelectionCell
+            :value="
+              writableList.every((row) => row.checked) &&
+              writableList.length != 0
+            "
+            :intermediate="
+              writableList.some((row) => row.checked) &&
+              !(
+                writableList.every((row) => row.checked) &&
+                writableList.length != 0
+              )
+            "
+            @change="
+              (value) => {
+                writableList.forEach((log) => {
+                  if (log.status == 'ready' || log.status == 'conflict') {
+                    log.checked = value;
+                  }
+                });
+              }
+            "
+            :disabled="writableList.length == 0"
+          />
+        </template>
+      </el-table-column>
+      <el-table-column prop="filename" label="文件名" :width="350" />
+      <el-table-column prop="status" label="状态" :width="100">
+        <template #header="{ column }">
           <div
-            class="el-loading-mask"
             style="display: flex; align-items: center; justify-content: center"
           >
-            <el-icon
-              class="is-loading"
-              color="var(--el-color-primary)"
-              :size="26"
+            <span
+              style="
+                margin-right: 0.5rem;
+                font-size: 0.75rem;
+                line-height: 1rem;
+              "
             >
-              <loading-icon />
-            </el-icon>
+              {{ column.title }}
+            </span>
+            <ElPopover ref="popoverRef" trigger="click" width="150">
+              <div class="filter-wrapper">
+                <div class="filter-group">
+                  <ElCheckbox v-model="shouldFilter.ready">Ready</ElCheckbox>
+                  <ElCheckbox v-model="shouldFilter.conflict">
+                    Conflict
+                  </ElCheckbox>
+                  <ElCheckbox v-model="shouldFilter.ignore">
+                    Ignore
+                  </ElCheckbox>
+                  <ElCheckbox v-model="shouldFilter.success">
+                    Success
+                  </ElCheckbox>
+                  <ElCheckbox v-model="shouldFilter.fault">Fault</ElCheckbox>
+                </div>
+                <div
+                  style="
+                    border-top: 1px solid #4c4d4f;
+                    margin: 12px -12px -12px;
+                    padding: 0 12px;
+                    display: flex;
+                    justify-content: center;
+                  "
+                >
+                  <ElButton text @click="onReset"> Reset </ElButton>
+                </div>
+              </div>
+              <template #reference>
+                <ElIcon style="cursor: pointer">
+                  <Filter />
+                </ElIcon>
+              </template>
+            </ElPopover>
           </div>
         </template>
-        <template #empty>
-          <el-empty :image-size="120" />
-        </template>
-      </el-table-v2>
-    </template>
-  </el-auto-resizer>
+      </el-table-column>
+      <el-table-column prop="message" label="说明" :width="500" />
+    </el-table>
+    <el-pagination
+      style="justify-content: center; margin-top: 10px"
+      background
+      layout="total, prev, pager, next"
+      v-model:current-page="writableCurPage"
+      :total="writableList.length"
+      :page-size="pageSize"
+      :pager-count="5"
+      small
+    />
+  </div>
 </template>
 <script setup>
-import { Loading as LoadingIcon, Filter } from "@element-plus/icons-vue";
+import { Filter } from "@element-plus/icons-vue";
 import { ref, computed, reactive } from "vue";
 import { ElCheckbox, ElButton, ElIcon, ElPopover } from "element-plus";
 
@@ -56,6 +141,14 @@ const props = defineProps({
   loading: Boolean,
   list: Array,
   selected: Array,
+});
+const pageSize = 100;
+const writableCurPage = ref(1);
+const showList = computed(() => {
+  return writableList.value.slice(
+    (writableCurPage.value - 1) * pageSize,
+    writableCurPage.value * pageSize
+  );
 });
 const writableList = computed({
   get: () => {
@@ -90,108 +183,6 @@ const SelectionCell = ({ value, intermediate = false, onChange, disabled }) => {
   );
 };
 
-const columns = [
-  {
-    key: "selection",
-    width: 50,
-    cellRenderer: ({ rowData }) => {
-      const onChange = (value) => {
-        if (rowData.status == "ready" || rowData.status == "conflict") {
-          rowData.checked = value;
-        }
-      };
-      return (
-        <SelectionCell
-          value={rowData.checked}
-          onChange={onChange}
-          disabled={rowData.status != "ready" && rowData.status != "conflict"}
-        />
-      );
-    },
-
-    headerCellRenderer: () => {
-      const onChange = (value) => {
-        writableList.value.forEach((log) => {
-          if (log.status == "ready" || log.status == "conflict") {
-            log.checked = value;
-          }
-        });
-      };
-      const allSelected =
-        writableList.value.every((row) => row.checked) &&
-        writableList.value.length != 0;
-      const containsChecked = writableList.value.some((row) => row.checked);
-      const isDisabled = writableList.value.length == 0;
-
-      return (
-        <SelectionCell
-          value={allSelected}
-          intermediate={containsChecked && !allSelected}
-          onChange={onChange}
-          disabled={isDisabled}
-        />
-      );
-    },
-  },
-  {
-    key: "filename",
-    title: "文件名",
-    width: 350,
-    dataKey: "filename",
-  },
-  {
-    key: "status",
-    title: "状态",
-    width: 100,
-    dataKey: "status",
-    headerCellRenderer: (props) => {
-      return (
-        <div style="display:flex;align-items:center;justify-content:center;">
-          <span style="margin-right:0.5rem;font-size:0.75rem;line-height:1rem;">
-            {props.column.title}
-          </span>
-          <ElPopover ref={popoverRef} trigger="click" {...{ width: 150 }}>
-            {{
-              default: () => (
-                <div class="filter-wrapper">
-                  <div class="filter-group">
-                    <ElCheckbox v-model={shouldFilter.ready}>Ready</ElCheckbox>
-                    <ElCheckbox v-model={shouldFilter.conflict}>
-                      Conflict
-                    </ElCheckbox>
-                    <ElCheckbox v-model={shouldFilter.ignore}>
-                      Ignore
-                    </ElCheckbox>
-                    <ElCheckbox v-model={shouldFilter.success}>
-                      Success
-                    </ElCheckbox>
-                    <ElCheckbox v-model={shouldFilter.fault}>Fault</ElCheckbox>
-                  </div>
-                  <div style="border-top: 1px solid #4C4D4F;margin: 12px -12px -12px;padding: 0 12px;display: flex;justify-content: center;">
-                    <ElButton text onClick={onReset}>
-                      Reset
-                    </ElButton>
-                  </div>
-                </div>
-              ),
-              reference: () => (
-                <ElIcon style="cursor:pointer;">
-                  <Filter />
-                </ElIcon>
-              ),
-            }}
-          </ElPopover>
-        </div>
-      );
-    },
-  },
-  {
-    key: "message",
-    title: "说明",
-    width: 500,
-    dataKey: "message",
-  },
-];
 const popoverRef = ref();
 
 const onReset = () => {
